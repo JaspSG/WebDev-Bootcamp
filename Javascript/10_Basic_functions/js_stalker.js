fb42e8c5deef751b7a72312da4b8819cda30fe6c
JaspSG <54952087+JaspSG@users.noreply.github.com>
2020-05-14 00:15:08 +0800

    Add files via upload

---
+++ Javascript/10_Basic_functions/js_stalker.js
@@ -0,0 +1,10 @@
+const firstName = prompt("What is your first name?")
+const lastName = prompt("What is your last name?")
+const age = prompt("What is your age?")
+
+console.log("Name: "+ firstName+ " " +lastName)
+console.log("Your age is: " + age)
+
+
+alert("Name: "+ firstName+ " " +lastName)
+alert("Your age is: " + age)
