e94d7f7c81376290d8e845c94e7ce4c6d6c69c73
SinclairSGx <jasperlbh93@Hotmail.com>
2020-05-22 23:46:02 +0800

    Jquery Stuff

---
+++ Javascript/J_Query/jQueryExercise/exercise.js
@@ -0,0 +1,10 @@
+// Selecting
+
+$("div").css("background", "purple");
+
+$("div.highlight").css("width", "200px");
+
+$("div#third").css("border", "4px solid orange");
+
+// Bonus: Select the first div only and change its font color to pin
+$("div:first-of-type").css("color", "pink");
