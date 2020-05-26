b724f5a1018f69862eef51ac6c36a2d99eccbfd9
SinclairSGx <jasperlbh93@Hotmail.com>
2020-05-26 21:26:19 +0800

    completed Todo, created patatap folder

--- Javascript/Todo_Jquery/assets/js/todo.js
+++ Javascript/Todo_Jquery/assets/js/todo.js
@@ -5,25 +5,32 @@
 // });
 
 $("ul").on("click", "li", function () {
-    $(this).toggleClass("completed");
+  $(this).toggleClass("completed");
 });
 
 //Click on X to delete Todos
 $("ul").on("click", "span", function (event) {
-    //remove the entire li instead of just the X with parent
-    $(this).parent().fadeOut(500, function(){
-        $(this).remove(); //$(this) refers to parent element
+  //remove the entire li instead of just the X with parent
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove(); //$(this) refers to parent element
     });
-    //prevents from trigerring multiple events due to nested elements
-    event.stopPropagation();
-  });
+  //prevents from trigerring multiple events due to nested elements
+  event.stopPropagation();
+});
 
+$("input[type='text']").keypress(function (e) {
+  if (event.which === 13) {
+    //grabbing todo text
+    let todoText = $(this).val();
+    $(this).val(""); //set input field to empty after grabbing value
+    $("ul").append(
+      "<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>"
+    );
+  }
+});
 
-  $("input[type='text']").keypress(function (e) { 
-      if(event.which === 13){
-          //grabbing todo text
-          let todoText = $(this).val();
-          $(this).val(""); //set input field to empty after grabbing value
-          $("ul").append("<li><span>X</span> " + todoText + "</li>");
-      }
-  });
+$(".fa-plus").click(function () {
+  $("input[type ='text']").fadeToggle();
+});
